b3c244d65800f20d94f789742b7a647b072ac783
raiviss <rstudens@gmail.com>
2023-05-14 16:38:55 +0300

    answers interface and model

---
+++ backend/src/interfaces/answer.interface.ts
@@ -0,0 +1,7 @@
+export interface Answer {
+    id: number;
+    questionId: number;
+    text: string;
+    introvertScore: number;
+    extrovertScore: number;
+}
---
+++ backend/src/models/answers.model.ts
@@ -0,0 +1,25 @@
+import { Answer } from '@/interfaces/answer.interface';
+
+
+export const AnswerModel: Answer[] = [
+  { id: 1, questionId: 1, text: 'Don’t dare to interrupt them',  introvertScore: 50, extrovertScore: 0 },
+  { id: 2, questionId: 1, text: 'Think it’s more important to give them some of your time; work can wait', introvertScore: 50, extrovertScore: 0 },
+  { id: 3, questionId: 1, text: 'Listen, but with only with half an ear',  introvertScore: 0, extrovertScore: 25 },
+  { id: 4, questionId: 1, text: 'Interrupt and explain that you are really busy at the moment',  introvertScore: 0, extrovertScore: 50 },
+  { id: 5, questionId: 2, text: 'Look at your watch every two minutes',  introvertScore: 50, extrovertScore: 0 },
+  { id: 6, questionId: 2, text: 'Bubble with inner anger, but keep quiet',  introvertScore: 40, extrovertScore: 0 },
+  { id: 7, questionId: 2, text: 'Explain to other equally impatient people in the room that the doctor is always running late',  introvertScore: 0, extrovertScore: 35 },
+  { id: 8, questionId: 2, text: 'Complain in a loud voice, while tapping your foot impatiently',  introvertScore: 0, extrovertScore: 50 },
+  { id: 9, questionId: 3, text: 'Don’t dare contradict them',  introvertScore: 50, extrovertScore: 0 },
+  { id: 10, questionId: 3, text: 'Think that they are obviously right',  introvertScore: 50, extrovertScore: 0 },
+  { id: 11, questionId: 3, text: 'Defend your own point of view, tooth and nail',  introvertScore: 0, extrovertScore: 35 },
+  { id: 12, questionId: 3, text: 'Continuously interrupt your colleague',  introvertScore: 0, extrovertScore: 50 },
+  { id: 13, questionId: 4, text: 'Are a bit too far towards the back so don’t really hear what the guide is saying',  introvertScore: 50, extrovertScore: 0 },
+  { id: 14, questionId: 4, text: 'Follow the group without question',  introvertScore: 25, extrovertScore: 0 },
+  { id: 15, questionId: 4, text: 'Make sure that everyone is able to hear properly',  introvertScore: 0, extrovertScore: 25 },
+  { id: 16, questionId: 4, text: 'Are right up the front, adding your own comments in a loud voice',  introvertScore: 0, extrovertScore: 50 },
+  { id: 17, questionId: 5, text: 'Ask you to tell a story in front of everyone else',  introvertScore: 50, extrovertScore: 0 },
+  { id: 18, questionId: 5, text: 'Talk privately between themselves',  introvertScore: 0, extrovertScore: 25 },
+  { id: 19, questionId: 5, text: 'Hang around you all evening',  introvertScore: 25, extrovertScore: 0 },
+  { id: 20, questionId: 5, text: 'Always drag the conversation back to themselves',  introvertScore: 0, extrovertScore: 50 },
+];
