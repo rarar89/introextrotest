9c60f1dacacf1958584388e6f9a348db056a7543
raiviss <rstudens@gmail.com>
2023-05-14 16:53:24 +0300

    frontend- added daisyui lib and some styling

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1 className="text-2xl py-2">Are you introvert or extrovert?</h1>
       </header>
+      <div>
+        
+      </div>
     </div>
   );
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          'primary': '#661AE6',
+          'secondary': '#D926AA',
+          'accent': '#1FB2A5',
+          'neutral': '#191D24',
+          'base-100': '#2A303C',
+          'info': '#3ABFF8',
+          'success': '#36D399',
+          'warning': '#FBBD23',
+          'error': '#F87272',
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+};
