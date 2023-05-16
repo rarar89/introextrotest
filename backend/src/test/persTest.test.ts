7624813640b5454fcf0083e61c2013fb9149e466
raiviss <rstudens@gmail.com>
2023-05-16 09:36:19 +0300

    backend tests

---
+++ backend/src/test/persTest.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import { App } from '@/app';
+import { PersTestRoute } from '@/routes/persTest.route';
+import { QuestionModel } from '@/models/question.model';
+import { PersType } from '@/services/persTest.service';
+
+afterAll(async () => {
+  await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
+});
+
+describe('TEST PersTest API', () => {
+  const route = new PersTestRoute();
+  const app = new App([route]);
+
+  describe('[GET] /test/info', () => {
+    it('response statusCode 200 /getInfo', () => {
+
+      return request(app.getServer()).get(`${route.path}/info`).expect(200, { totalQuestions: QuestionModel.length });
+    });
+  });
+
+  describe('[GET] /test/type/:answers', () => {
+    it('response statusCode 200 /getType', () => {
+      
+      const answers = [1,5,10,15,20];
+
+      return request(app.getServer()).get(`${route.path}/type/${answers.join(',')}`).expect(200, { type: PersType.Introvert });
+    });
+  });
+});
---
+++ backend/src/test/question.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest';
+import { App } from '@/app';
+import { PersTestRoute } from '@/routes/persTest.route';
+import { QuestionModel } from '@/models/question.model';
+import { PersType } from '@/services/persTest.service';
+import { QuestionRoute } from '@/routes/question.route';
+
+afterAll(async () => {
+  await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
+});
+
+describe('TEST Question API', () => {
+  const route = new QuestionRoute();
+  const app = new App([route]);
+
+  describe('[GET] /question/:id', () => {
+    it('response statusCode 200 /getQuestion', () => {
+
+      const question = QuestionModel[0];
+
+      return request(app.getServer()).get(`${route.path}/${question.id}`).expect(200, question);
+    });
+  });
+});
